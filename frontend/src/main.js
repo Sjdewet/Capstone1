780a5802ccef8db96b26d44d17a29baa5b4195a7
Sjdewet <summerdewet@gmail.com>
2023-08-29 16:26:33 +0200

    viewSetup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/style.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import { MotionPlugin } from '@vueuse/motion'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(MotionPlugin).mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import AdminView from '../views/AdminView.vue'
+import ProductsView from '../views/ProductsView.vue'
+import ContactView from '../views/ContactView.vue'
+import CheckoutView from '../views/CheckoutView.vue'
+import ViewProfileView from '../views/ViewProfileView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const routes = [
   {
@@ -10,11 +17,45 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
+  },
+
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: CheckoutView
+  },
+
+
+  {
+    path: '/ViewProfile',
+    name: 'ViewProfile',
+    component: ViewProfileView
+  },
+
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
   }
+
+
+
 ]
 
 const router = createRouter({
@@ -23,3 +64,4 @@ const router = createRouter({
 })
 
 export default router
+
